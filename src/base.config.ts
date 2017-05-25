2b95568127092d85cee7f28237665f813a38a3f1
sven <sven199109@gmail.com>
2017-05-25 18:49:22 +0800

    rename to assets

--- src/base.config.ts
+++ src/base.config.ts
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 8080
 const url = `http://localhost:${PORT}/webpack-dev-server/index.html`
 const productionURL = path.join('file://', __dirname, './index.html')
 const developmentURL = 'http://localhost:' + PORT
-const iconPath = path.join(__dirname, '../public/images/logo.png')
+const iconPath = path.join(__dirname, '../assets/images/logo.png')
 
 const NODE_ENV = function() {
   return process.env.NODE_ENV || 'development'
